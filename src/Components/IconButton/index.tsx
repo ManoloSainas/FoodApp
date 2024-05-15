66a3cf477f2e84b337cc064e8d075425fe75dec2
Manolo Sainas <sainas.manolo@gmail.com>
2024-05-15 10:16:02 +0200

    edit

--- src/Components/IconButton/index.tsx
+++ src/Components/IconButton/index.tsx
@@ -1,21 +1,22 @@
 import { Icon } from '../Icon'
 import { ButtonVariant } from '../Button/styled'
-import { PropsWithChildren } from 'react'
+import { ReactElement } from 'react'
 import { IconName } from '../Icon/config'
 import { FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
 import { Button } from '../Button'
 
-type Props = PropsWithChildren<{
+type Props = {
   onClick: () => void
   disabled?: boolean
   variant?: ButtonVariant
   iconName: IconName
   color?: string
   size?: FontAwesomeIconProps['size']
-}>
+  text?: string
+  children: ReactElement<Icon>
+}
 
 export const IconButton = ({
-  children,
   onClick,
   disabled,
   variant = 'primary',
@@ -25,8 +26,10 @@ export const IconButton = ({
 }: Props) => {
   return (
     <Button onClick={onClick} disabled={disabled} variant={variant}>
-      {children}
-      <Icon iconName={iconName} color={color} size={size}></Icon>
+      {text}
+      {<Icon iconName={iconName} color={color} size={size}></Icon>}
     </Button>
   )
 }
+
+// creare styled per il div
