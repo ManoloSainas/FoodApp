8a6a3937470b900961fae88148797fbbb484ebca
Manolo Sainas <sainas.manolo@gmail.com>
2024-05-29 12:11:38 +0200

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import './App.css'
-import { IconName } from './Components/Icon/config'
-import { SideBar } from './Components/SideBar'
 
 function App() {
   return (
-    <SideBar
-      onChange={(icon: IconName) => console.log(icon)}
-      options={[
-        { icon: 'Home', text: 'Home' },
-        { icon: 'Catalog', text: 'Catalog' }
-      ]}
-    />
-  )
+    
+  );
 }
 
 export default App
