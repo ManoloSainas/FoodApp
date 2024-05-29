b30d9a62630c25e6a03395252a7006a43a4c3873
Manolo Sainas <sainas.manolo@gmail.com>
2024-05-29 16:45:03 +0200

    edit

--- src/Components/SearchBar/styled.ts
+++ src/Components/SearchBar/styled.ts
@@ -5,12 +5,12 @@ export const StyledSearchBar = styled.div<{}>`
   display: flex;
   justify-content: space-between;
   background-color: #111114;
-  margin: 15px;
+  margin: 17px;
   padding: 2px 2px 2px 22px;
   border-top-right-radius: 50px 50px;
   border-bottom-right-radius: 50px 50px;
   border-top-left-radius: 50px 50px;
   border-bottom-left-radius: 50px 50px;
-  width: 300px;
+  width: 310px;
   color: #17171c;
 `
