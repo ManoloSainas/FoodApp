960f7c0a9a8c495be415172fd615f47c0e61ca19
Manolo Sainas <sainas.manolo@gmail.com>
2024-06-05 12:18:28 +0200

    edit

--- src/Pages/Home/HomeTopCards/index.tsx
+++ src/Pages/Home/HomeTopCards/index.tsx
@@ -1,41 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ImageTextCard } from '../../../Components/Cards/ImageTextCard'
 import { Stack } from '../../../Components/Stack'
 
 export const HomeTopCards = () => {
-  const [data, setData] = useState()
+  const [data, setData] = useState([
+    {
+      imageURL: 'src/assets/Images/banana.jpg',
+      text: 'banana'
+    },
+    {
+      imageURL: 'src/assets/Images/cherry.jpg',
+      text: 'cherry'
+    },
+    {
+      imageURL: 'src/assets/Images/orange.jpg',
+      text: 'orange'
+    },
+    {
+      imageURL: 'src/assets/Images/pineapple.jpg',
+      text: 'pineapple'
+    },
+    {
+      imageURL: 'src/assets/Images/strawberry.jpg',
+      text: 'strawberry'
+    },
+    {
+      imageURL: 'src/assets/Images/watermelon.jpg',
+      text: 'watermelon'
+    }
+  ])
+
+  useEffect(() => {
+    fetch('')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+  }, [])
 
   return (
     <Stack gap="17px" flexWrap="wrap" width="100%">
-      <ImageTextCard
-        onClick={(value) => console.log(value)}
-        cards={[
-          {
-            imageURL: 'src/assets/Images/banana.jpg',
-            text: 'banana'
-          },
-          {
-            imageURL: 'src/assets/Images/cherry.jpg',
-            text: 'cherry'
-          },
-          {
-            imageURL: 'src/assets/Images/orange.jpg',
-            text: 'orange'
-          },
-          {
-            imageURL: 'src/assets/Images/pineapple.jpg',
-            text: 'pineapple'
-          },
-          {
-            imageURL: 'src/assets/Images/strawberry.jpg',
-            text: 'strawberry'
-          },
-          {
-            imageURL: 'src/assets/Images/watermelon.jpg',
-            text: 'watermelon'
-          }
-        ]}
-      />
+      <ImageTextCard onClick={(value) => console.log(value)} cards={data} />
     </Stack>
   )
 }
