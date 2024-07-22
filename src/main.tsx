65fe5717f4c7220b788144e5eca25f19c0bbd1c2
Manolo Sainas <sainas.manolo@gmail.com>
2024-07-22 10:21:46 +0200

    Redux Deleted

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import store from './app/store.ts'
-import { Provider } from 'react-redux'
 
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 )
